95e6cc46b1dedb05dde5c14b097ca685a9484bd5
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-08-17 22:03:21 +0300

    apply tailwind and flowbite

--- src/app/layout/additions/navbar/navbar.component.ts
+++ src/app/layout/additions/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../shared/services/auth/auth.service';
+import { FlowbiteService } from '../../../shared/services/flowbite/flowbite.service';
 
 @Component({
   selector: 'app-navbar',
@@ -12,9 +13,15 @@ import { AuthService } from '../../../shared/services/auth/auth.service';
 export class NavbarComponent implements OnInit {
   isLogin: boolean = false;
 
-  constructor(public _AuthService: AuthService) {}
+  constructor(public _AuthService: AuthService,private _FlowbiteService:FlowbiteService) {}
 
   ngOnInit(): void {
+
+    this._FlowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+
     this._AuthService.userData.subscribe(() => {
       if (this._AuthService.userData.getValue() != null) {
         this.isLogin = true;
---
+++ src/app/shared/services/flowbite/flowbite.service.ts
@@ -0,0 +1,17 @@
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlowbiteService {
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  loadFlowbite(callback: (flowbite: any) => void) {
+    if (isPlatformBrowser(this.platformId)) {
+      import('flowbite').then(flowbite => {
+        callback(flowbite);
+      });
+    }
+  }
+}
