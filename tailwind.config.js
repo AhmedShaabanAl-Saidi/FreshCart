1cb13f255d57a2508364eda7d5af592c5808d6c0
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-08-23 04:31:31 +0300

    media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,18 @@ module.exports = {
     "./node_modules/flowbite/**/*.js" 
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '2rem',
+        xl: '3rem',
+        '2xl': '4rem',
+      },
+    },
     extend: {},
   },
   plugins: [
     require('flowbite/plugin')
   ],
 }
-
