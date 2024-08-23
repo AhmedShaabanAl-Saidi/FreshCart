5f970207b60c26a0abc9107fa7e8a6771580f3b0
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-08-23 15:07:51 +0300

    media

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-    "./node_modules/flowbite/**/*.js" 
-  ],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     container: {
       padding: {
-        DEFAULT: '1rem',
-        sm: '1rem',
-        lg: '2rem',
-        xl: '3rem',
-        '2xl': '4rem',
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "2rem",
+        xl: "3rem",
+        "2xl": "4rem",
       },
     },
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
