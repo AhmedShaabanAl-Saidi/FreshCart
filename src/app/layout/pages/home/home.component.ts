133773b0b82fc54b8d3f06132623ddfa3f2cbce2
AhmedShaabanAl-Saidi <ahmedshaaban123321@gmail.com>
2024-08-29 11:21:38 +0300

    products component

--- src/app/layout/pages/home/home.component.ts
+++ src/app/layout/pages/home/home.component.ts
@@ -1,71 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../../shared/services/product/product.service';
-import { product } from '../../../shared/interfaces/product';
 import { CategorysliderComponent } from '../../additions/categoryslider/categoryslider.component';
 import { HomesliderComponent } from '../../additions/homeslider/homeslider.component';
-import { RouterLink } from '@angular/router';
-import { SearchPipe } from '../../../shared/pipes/search.pipe';
-import { FormsModule } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
-import { CartService } from '../../../shared/services/cart/cart.service';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [
-    CategorysliderComponent,
-    HomesliderComponent,
-    RouterLink,
-    SearchPipe,
-    FormsModule,
-  ],
+  imports: [CategorysliderComponent, HomesliderComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  productList!: product[];
-  isLoading: boolean = false;
-  userInput: string = '';
-
-  constructor(
-    private _ProductService: ProductService,
-    private toastr: ToastrService,
-    private _CartService: CartService
-  ) {}
-
   ngOnInit(): void {
     if (typeof localStorage != 'undefined') {
       localStorage.setItem('currentPage', '/home');
     }
-
-    this.getAllProducts();
-  }
-
-  getAllProducts() {
-    this.isLoading = true;
-    this._ProductService.getAllProduct().subscribe({
-      next: (res) => {
-        this.productList = res.data;
-        // console.log(this.productList);
-        this.isLoading = false;
-      },
-      error: (err) => {
-        console.log(err);
-        this.isLoading = false;
-      },
-    });
-  }
-
-  addProductToCart(productId: string) {
-    this._CartService.addProductToCart(productId).subscribe({
-      next: (res) => {
-        // console.log(res);
-        if ('status' in res) {
-          this.toastr.success(res.message, res.status, {
-            progressBar: true,
-          });
-        }
-      },
-    });
   }
 }
--- src/app/layout/pages/products/products.component.ts
+++ src/app/layout/pages/products/products.component.ts
@@ -1,16 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { product } from '../../../shared/interfaces/product';
+import { ToastrService } from 'ngx-toastr';
+import { CartService } from '../../../shared/services/cart/cart.service';
+import { ProductService } from '../../../shared/services/product/product.service';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { SearchPipe } from '../../../shared/pipes/search.pipe';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,
+    SearchPipe,
+    FormsModule,],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit{
+  productList!: product[];
+  isLoading: boolean = false;
+  userInput: string = '';
+
+  constructor(
+    private _ProductService: ProductService,
+    private toastr: ToastrService,
+    private _CartService: CartService
+  ) {}
+
   ngOnInit(): void {
     if (typeof localStorage != 'undefined') {
       localStorage.setItem('currentPage', '/products');
     }
+
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.isLoading = true;
+    this._ProductService.getAllProduct().subscribe({
+      next: (res) => {
+        this.productList = res.data;
+        // console.log(this.productList);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.log(err);
+        this.isLoading = false;
+      },
+    });
+  }
+
+  addProductToCart(productId: string) {
+    this._CartService.addProductToCart(productId).subscribe({
+      next: (res) => {
+        // console.log(res);
+        if ('status' in res) {
+          this.toastr.success(res.message, res.status, {
+            progressBar: true,
+          });
+        }
+      },
+    });
   }
 }
